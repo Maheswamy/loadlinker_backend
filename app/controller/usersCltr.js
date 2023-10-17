404f840d4c12e0df69bd0c9957e39584f136d34e
Maheswamy <mahendragowdas1997@gmail.com>
2023-10-17 13:06:47 +0530

    registration completed

--- app/controller/usersCltr.js
+++ app/controller/usersCltr.js
@@ -3,20 +3,19 @@ const _ = require("lodash");
 const jwt = require("jsonwebtoken");
 const bcryptjs = require("bcryptjs");
 const { validationResult } = require("express-validator");
-const otpSender = require("../helper/otpSender");
+const transporter = require("../helper/emailVerification");
 
 const usersCltr = {};
 
 const saltAndHash = async (value) => {
   const salt = await bcryptjs.genSalt();
-  const hashedOtp = await bcryptjs.hash(value, salt);
-  return hashedOtp;
+  const hashed = await bcryptjs.hash(value, salt);
+  return hashed;
 };
 
-const generateOTP = async (mobileNumber) => {
+const generateOTP = () => {
   const otp = `${Math.floor(100000 + Math.random() * 900000)}`;
-  otpSender(otp, mobileNumber);
-  return saltAndHash(otp);
+  return otp;
 };
 
 usersCltr.register = async (req, res) => {
@@ -38,16 +37,33 @@ usersCltr.register = async (req, res) => {
       body.role = "admin";
     }
     body.password = await saltAndHash(body.password);
-    body.otp = await generateOTP(body.mobileNumber);
-    const newUser = await new User(body).save();
+    const otp = generateOTP();
 
-    res.json({
-      message: "Otp sent to mobile number",
-      name: newUser.firstName + newUser.lastName,
+    const mailOptions = {
+      from: "mahendragowdas1997@gmail.com",
+      to: body.email,
+      subject: `${body.firstName} ${body.lastName} OTP Verification Code`,
+      text: `please use the following OTP code:${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, async (error, info) => {
+      if (error) {
+        return res.status(500).json({ error: "Error sending email" });
+      } else {
+        body.otp = await saltAndHash(otp);
+        const newUser = await new User(body).save();
+        return res
+          .status(200)
+          .json({ message: "Email sent successfully", name: newUser.email });
+      }
     });
   } catch (e) {
+    console.log(e);
     res.status(500).json(e);
   }
 };
 
 module.exports = usersCltr;
+
+
+
